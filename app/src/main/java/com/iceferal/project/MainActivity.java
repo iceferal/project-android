bd7e208a60debeccebbd4495bd1b6d66cd523642
mr.alien <mr.alien@poczta.fm>
2020-05-20 13:56:06 +0200

    logo

--- app/src/main/java/com/iceferal/project/MainActivity.java
+++ app/src/main/java/com/iceferal/project/MainActivity.java
@@ -2,9 +2,15 @@ package com.iceferal.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 3000;
@@ -13,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -21,6 +28,5 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }, SPLASH_TIME_OUT);
-
     }
 }
