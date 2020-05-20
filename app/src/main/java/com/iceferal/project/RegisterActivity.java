6ba3367a1c821bda7f08872f6755f1eae6d6d5d3
mr.alien <mr.alien@poczta.fm>
2020-05-20 22:35:34 +0200

    register

--- app/src/main/java/com/iceferal/project/RegisterActivity.java
+++ app/src/main/java/com/iceferal/project/RegisterActivity.java
@@ -1,6 +1,48 @@
 package com.iceferal.project;
 
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText login, email, pass, repass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        login = findViewById(R.id.login);
+        pass = findViewById(R.id.pass);
+        repass = findViewById(R.id.rePass);
+        email = findViewById(R.id.email);
+
+    }
+
+    public void klik(View view) {
+        boolean status = false;
+        String log = login.getText().toString();
+        String password = pass.getText().toString();
+        String repassword = repass.getText().toString();
+        String mail = email.getText().toString();
+
+        if (TextUtils.isEmpty(log)) {
+            login.setError("Pole haslo nie moze byc puste");
+            status = false;        }
+        if (TextUtils.isEmpty(password)) {
+            pass.setError("Pole haslo nie moze byc puste");
+            status = false;         }
+        if (TextUtils.isEmpty(repassword)) {
+            repass.setError("Pole haslo nie moze byc puste");
+            status = false;        }
+        if (TextUtils.isEmpty(mail)) {
+            email.setError("Pole haslo nie moze byc puste");
+            status = false;         }
+
+
+    }
 }
