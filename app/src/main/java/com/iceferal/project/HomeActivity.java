19378fb63ecb6ef17924e32a52193a2d2c4c0a78
mr.alien <mr.alien@poczta.fm>
2020-07-29 23:16:00 +0200

    clear home

--- app/src/main/java/com/iceferal/project/HomeActivity.java
+++ app/src/main/java/com/iceferal/project/HomeActivity.java
@@ -38,25 +38,8 @@ import com.iceferal.project.models.User;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
-//    private static final String TAG = com.example.currentplacedetailsonmap.MapsActivityCurrentPlace.class.getSimpleName();
     private DrawerLayout drawer;
-    private GoogleMap map;
-    private CameraPosition cameraPosition;
 
-    private PlacesClient placesClient;
-    private FusedLocationProviderClient fusedLocationProviderClient;
-    private final LatLng defaultLocation = new LatLng(-33.8523341, 151.2106085);
-    private static final int DEFAULT_ZOOM = 15;
-    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
-    private boolean locationPermissionGranted;
-    private Location lastKnownLocation;
-    private static final String KEY_CAMERA_POSITION = "camera_position";
-    private static final String KEY_LOCATION = "location";
-    private static final int M_MAX_ENTRIES = 5;
-    private String[] likelyPlaceNames;
-    private String[] likelyPlaceAddresses;
-    private List[] likelyPlaceAttributions;
-    private LatLng[] likelyPlaceLatLngs;
 
 
     @Override
@@ -79,14 +62,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
 //^^ up menu ^^
 
-        if (savedInstanceState != null) {
-            lastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
-            cameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);        }
-
-        Places.initialize(getApplicationContext(), getString(R.string.maps_api_key));
-        placesClient = Places.createClient(this);
-
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+//
 }
 //menu function
     @Override
@@ -132,43 +108,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         }    }
 
 
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//        this.map = map;
-//        this.map.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
-//
-//            @Override
-//            public View getInfoWindow(Marker arg0) {
-//                return null;            }
-//
-//            @Override
-//            public View getInfoContents(Marker marker) {
-//                View infoWindow = getLayoutInflater().inflate(R.layout.custom_info_contents,
-//                        (FrameLayout) findViewById(R.id.mapView), false);
-//
-//                TextView title = infoWindow.findViewById(R.id.title);
-//                title.setText(marker.getTitle());
-//
-//                TextView snippet = infoWindow.findViewById(R.id.snippet);
-//                snippet.setText(marker.getSnippet());
-//
-//                return infoWindow;            }
-//        });
-//
-//        getLocationPermission();
-//        updateLocationUI();
-//        getDeviceLocation();
-//    }
 
-    private void updateLocationUI() {
-    }
-
-    private void getLocationPermission() {
-    }
-
-    private void getDeviceLocation() {
-
-    }
 
     public void userLogout() {
         LoginActivity.disconnectFromFacebook();
