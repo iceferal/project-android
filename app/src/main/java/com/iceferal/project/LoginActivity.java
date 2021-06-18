79eaa56fca2c3852552851d72143e23be0b33961
iceferal <iceferal@gmail.com>
2021-06-18 18:37:13 +0200

    gps repair

--- app/src/main/java/com/iceferal/project/LoginActivity.java
+++ app/src/main/java/com/iceferal/project/LoginActivity.java
@@ -5,11 +5,14 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.graphics.Color;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.Spanned;
@@ -86,6 +89,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         callbackManager = CallbackManager.Factory.create();
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         login = findViewById(R.id.login_text);
         password = findViewById(R.id.pass_text);
@@ -112,6 +116,9 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         textView.setMovementMethod(LinkMovementMethod.getInstance());
 
 
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            showGPSDisabledAlertToUser();
+        }
 
 // google login
         googleSignInButton = findViewById(R.id.google);
@@ -347,6 +354,27 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         });
     }
 
+    private void showGPSDisabledAlertToUser(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("GPS jest wyłączony. Chcesz go włączyć?")
+                .setCancelable(false)
+                .setPositiveButton("Włącz likalizację GPS",
+                        new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int id){
+                                Intent callGPSSettingIntent = new Intent(
+                                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(callGPSSettingIntent);
+                            }
+                        });
+        alertDialogBuilder.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
 }
 
 
